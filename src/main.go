baf250e5677f39b8b72aecccdfaa8105ac01e037
Greg Fisher <greg@gnfisher.com>
2023-04-06 15:40:57 -0400

    Add source for instructions

--- src/main.go
+++ src/main.go
@@ -1,12 +1,13 @@
 package main
 
-// To set this up I had to do the following:
 // - in ./src run `go mod init main`
 // - in ./src/learning_go `go mod init learning_go`
 // Because this is a local file not a published package, I had to then do:
 // - in ./src/main `go mod edit -replace learning_go=./learning_go`
 // - in ./src/main `go mod tidy` - to "syncronize the dependencies from learning_go module"
 // Then, the makefile commands to build and run the `src/main.go` file (or binary after building)
+// To set this up I had to do the following.
+// Source: https://go.dev/doc/tutorial/call-module-code
 import "learning_go"
 
 func main() {
