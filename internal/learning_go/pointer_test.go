80d90d0ae2f1d65e7e9f71cec6787fb78a325943
Greg Fisher <greg@gnfisher.com>
2023-05-02 18:03:05 -0400

    gofmt

--- internal/learning_go/pointer_test.go
+++ internal/learning_go/pointer_test.go
@@ -2,37 +2,36 @@ package learning_go
 
 import (
 	"testing"
-	
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestPointerInstanceIsNotMutable(t *testing.T) {
 	// You can pass a pointer to a function. The function gets a _copy_ of the pointer. You can mutate
-  // the memory it points to but you can't mutate the pointer itself, as it is a copy.
+	// the memory it points to but you can't mutate the pointer itself, as it is a copy.
 
-  tryToMutatePointerInstance := func(s *string){
-    x := "New String"
+	tryToMutatePointerInstance := func(s *string) {
+		x := "New String"
 
-    // Try to assign the pointer we passed in to a new pointer instance.
-    s  = &x 
-  }
+		// Try to assign the pointer we passed in to a new pointer instance.
+		s = &x
+	}
 
-  x := "Hello"
-  tryToMutatePointerInstance(&x)
+	x := "Hello"
+	tryToMutatePointerInstance(&x)
 
-  // See that we didn't actually change anything
-  require.Equal(t, "Hello", x)
+	// See that we didn't actually change anything
+	require.Equal(t, "Hello", x)
 
-  // But we can mutate the memory it points to
-  mutateWhatItPointsTo := func(s *string){
-    // Dereferencing. We're putting "New String" in the memory location that the passed in pointer
-    // pointed to.
-    *s = "New String"
-  }
+	// But we can mutate the memory it points to
+	mutateWhatItPointsTo := func(s *string) {
+		// Dereferencing. We're putting "New String" in the memory location that the passed in pointer
+		// pointed to.
+		*s = "New String"
+	}
 
-  mutateWhatItPointsTo(&x)
+	mutateWhatItPointsTo(&x)
 
-  // Now we've mutated things.
-  require.Equal(t, "New String", x)
+	// Now we've mutated things.
+	require.Equal(t, "New String", x)
 }
-
