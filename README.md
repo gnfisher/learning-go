1ba06038daf5034a0b0163c86d5c5a234b029372
Greg Fisher <greg@gnfisher.com>
2023-05-02 17:01:15 -0400

    Organize this like a real go project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code from reading through Learning Go
+
---
+++ cmd/learning_go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gnfisher/learning_go/internal/learning_go"
+)
+
+func main() {
+	learning_go.SayHello()
+	learning_go.ChapterTwo()
+	learning_go.SliceAppend()
+	learning_go.SliceExpression()
+	learning_go.SliceArrayExp()
+	learning_go.StringSlices()
+	learning_go.Maps()
+	learning_go.Structs()
+
+	learning_go.Play()
+
+	learning_go.PointerPlay()
+}
