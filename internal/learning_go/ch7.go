c056008301a03050eaf5f34c8cd89d00fb32b5d6
Greg Fisher <greg@gnfisher.com>
2023-05-03 16:23:33 -0400

    Handle nil receiver

--- internal/learning_go/ch7.go
+++ internal/learning_go/ch7.go
@@ -1,12 +1,29 @@
 package learning_go
 
+import (
+	"errors"
+)
+
 type Team struct {
 	Name    string
 	Players []string
 }
 
-func (t *Team) AddPlayer(name string) {
+func (t *Team) AddPlayer(name string) (err error, val bool) {
+	if t == nil {
+		err = errors.New("team is nil")
+		return err, val
+	}
+
+	for _, v := range t.Players {
+		if v == name {
+			val = true
+			return err, val
+		}
+	}
 	t.Players = append(t.Players, name)
+	val = true
+	return err, val
 }
 
 func (t *Team) RemovePlayer(name string) {
--- internal/learning_go/team_test.go
+++ internal/learning_go/team_test.go
@@ -12,6 +12,23 @@ func TestAddPlayer(t *testing.T) {
 	require.Equal(t, 1, len(team.Players))
 }
 
+func TestAddPlayerThatExists(t *testing.T) {
+	team := Team{
+		Name:    "Team1",
+		Players: []string{"Player1"},
+	}
+
+	team.AddPlayer("Player1")
+
+	require.Equal(t, 1, len(team.Players))
+}
+
+func TestAddPlayerToNil(t *testing.T) {
+	var team *Team
+	err, _ := team.AddPlayer("Player1")
+	require.NotNil(t, err)
+}
+
 func TestRemovePlayer(t *testing.T) {
 	team := Team{
 		Name:    "Team1",
