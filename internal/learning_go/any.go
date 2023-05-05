4e58d55664999ae5f55f5b15be742f49155e8fc2
Greg Fisher <greg@gnfisher.com>
2023-05-05 19:11:43 -0400

    Interfaces, functions, etc

--- internal/learning_go/any.go
+++ internal/learning_go/any.go
@@ -1,6 +1,9 @@
 package learning_go
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Any struct {
 	Value interface{}
@@ -8,4 +11,48 @@ type Any struct {
 
 func main() {
 	fmt.Println("Hello, World!")
+	h := HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	h.ServeHTTP(nil, nil)
+}
+
+type Handler interface {
+	ServeHTTP(http.ResponseWriter, *http.Request)
+}
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+func (f HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	f(w, r)
+}
+
+type MyHandler struct {
+	Data string
+}
+
+func (mh MyHandler) HelperFunction() string {
+	return "Helped"
+}
+
+func (mh MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	callMyHelper := mh.HelperFunction()
+	useMyData := mh.Data
+	// Do server stuff
+	fmt.Println(callMyHelper, useMyData)
+}
+
+func requiresHandlerInterface(h Handler) {
+	fmt.Println(h)
+}
+
+func anyFuncLikeThis(f func(w http.ResponseWriter, r *http.Request)) {
+}
+
+func doesItWork() {
+	h := MyHandler{
+		Data: "Hello, World!",
+	}
+	requiresHandlerInterface(h)
+	anyFuncLikeThis(h.ServeHTTP)
 }
